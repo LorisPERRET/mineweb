cd42d96fde8161a8e6576cd0638a5a441d4b7a37
Loris PERRET <Loris.PERRET@etu.uca.fr>
2022-11-19 09:37:50 +0100

    Fake data

--- MineWeb/Pages/Inventory.razor.cs
+++ MineWeb/Pages/Inventory.razor.cs
@@ -1,6 +1,21 @@
-﻿namespace MineWeb.Pages
+﻿using Microsoft.AspNetCore.Components;
+using MineWeb.Model;
+
+namespace MineWeb.Pages
 {
     public partial class Inventory
     {
+        private Item[] items;
+
+        [Inject]
+        public HttpClient Http { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            items = await Http.GetFromJsonAsync<Item[]>($"{NavigationManager.BaseUri}fake-data.json");
+        }
     }
 }
