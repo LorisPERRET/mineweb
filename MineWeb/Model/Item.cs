a520c499ecac7bc260f6596812c54bb221c9893f
Remi REGNAULT <Remi.REGNAULT@etu.uca.fr>
2022-11-19 09:28:47 +0100

    Class Item

---
+++ MineWeb/Model/Item.cs
@@ -0,0 +1,10 @@
+﻿namespace MineWeb.Model
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string DisplayName { get; set; }
+        public string Name { get; set; }
+        public int StackSize { get; set; }
+    }
+}
