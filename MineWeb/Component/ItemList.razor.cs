1974709c1fc3f07a3f5f94ba17871204dd60545b
Loris PERRET <Loris.PERRET@etu.uca.fr>
2022-11-19 09:58:36 +0100

    datagrid

---
+++ MineWeb/Component/ItemList.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using MineWeb.Model;
+
+namespace MineWeb.Component
+{
+    public partial class ItemList
+    {
+        private Item[] items;
+
+        [Inject]
+        public HttpClient Http { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            items = await Http.GetFromJsonAsync<Item[]>($"{NavigationManager.BaseUri}fake-data.json");
+        }
+    }
+}
--- MineWeb/Pages/Inventory.razor.cs
+++ MineWeb/Pages/Inventory.razor.cs
@@ -5,17 +5,6 @@ namespace MineWeb.Pages
 {
     public partial class Inventory
     {
-        private Item[] items;
-
-        [Inject]
-        public HttpClient Http { get; set; }
-
-        [Inject]
-        public NavigationManager NavigationManager { get; set; }
-
-        protected override async Task OnInitializedAsync()
-        {
-            items = await Http.GetFromJsonAsync<Item[]>($"{NavigationManager.BaseUri}fake-data.json");
-        }
+        
     }
 }
