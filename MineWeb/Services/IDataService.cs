ee8f68f7d56a58a04e99a888caf0eba00746188d
Loris PERRET <Loris.PERRET@etu.uca.fr>
2022-11-21 08:57:23 +0100

    List

---
+++ MineWeb/Services/IDataService.cs
@@ -0,0 +1,13 @@
+﻿using MineWeb.Model;
+
+namespace MineWeb.Services
+{
+    public interface IDataService
+    {
+        Task Add(ItemModel model);
+
+        Task<int> Count();
+
+        Task<List<Item>> List(int currentPage, int pageSize);
+    }
+}
