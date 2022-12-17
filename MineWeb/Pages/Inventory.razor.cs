c3c7f359199fb0851c37b39f95525c566dbaa7a0
Remi REGNAULT <Remi.REGNAULT@etu.uca.fr>
2022-12-17 08:59:48 +0100

    InventorySize

--- MineWeb/Pages/Inventory.razor.cs
+++ MineWeb/Pages/Inventory.razor.cs
@@ -11,6 +11,8 @@ namespace MineWeb.Pages
 
         private Dictionary<int,ItemForInventory> ItemsInventory = new Dictionary<int, ItemForInventory>();
 
+        public Item CurrentDragItem { get; set; }
+
         [Inject]
         public HttpClient Http { get; set; }
 
@@ -20,19 +22,21 @@ namespace MineWeb.Pages
         [Inject]
         public IStringLocalizer<Inventory> Localizer { get; set; }
 
-        public Item CurrentDragItem { get; set; }
+        [Inject]
+        public IConfiguration Configuration { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
             //items = await Http.GetFromJsonAsync<Item[]>($"{NavigationManager.BaseUri}fake-data.json");
 
-            Console.WriteLine($"{NavigationManager.BaseUri}fake-data.json");
+            int inventorySize = Configuration.GetSection("InventorySize").Get<int>();
+            Console.WriteLine(inventorySize);
 
             ItemForInventoryArchive[] liste = await Http.GetFromJsonAsync<ItemForInventoryArchive[]>($"{NavigationManager.BaseUri}save-data-inventory.json");
             
-            for (int i = 0; i < 27; i++)
+            for (int i = 0; i < inventorySize; i++)
             {
-                if (liste[i] == null)
+                if (i >= liste.Count() || liste[i] == null)
                 {
                     ItemsInventory.Add(i, null);
                 }
